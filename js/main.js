501bebead02644823248db9e4c5e60715f8067ce
Patrick Snowden <patrickbsnowden@outlook.com>
2022-04-11 23:36:54 -0700

    Added SwiperJS

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
 let input = document.querySelector('input')
-
+document.addEventListener('DOMContentLoaded', cocktail)
 input.addEventListener('keyup', e => {
     if(e.key === 'Enter'){
-        input = document.querySelector('input').value.split(' ').join('_')
+        cocktail()
+    }
+})
+
+function cocktail(){
+    input = document.querySelector('input').value.split(' ').join('_')
     const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${input}`
   console.log(url)
     fetch(url)
         .then(res => res.json()) // parse response as JSON
         .then(data => {
-           clear()
+            clear()
+        reinitSwiper(swiper)
             data.drinks.forEach((drink) => {
                 console.log(drink)
                 getInfo(drink) 
@@ -18,27 +24,43 @@ input.addEventListener('keyup', e => {
         .catch(err => {
             console.log(`error ${err}`)
         });
-    }
-})
-
-function clear(){
-    const main = document.querySelector('.container')
-    while(main.firstChild){
-        main.removeChild(main.firstChild)
-    }
 }
 
+function clear(){
+    const swiperWrapper = document.querySelector('.swiper-wrapper')
+    while(swiperWrapper.firstChild){
+        swiperWrapper.removeChild(swiperWrapper.firstChild)
+    }
+} 
+
 function getInfo(drink){
-    const section = document.createElement('section');
-    section.innerHTML = `
-    <h2>${drink.strDrink}</h2>
-    <img src="${drink.strDrinkThumb}"></img>
-    <h3>Ingredients</h3>
-    <span>${getIngredient(drink)}</span>
-    <h3>Instructions</h3>
-    <p>${drink.strInstructions}</p>
+    const div = document.createElement('div');
+    div.classList.add('swiper-slide')
+    div.classList.add('card')
+    div.innerHTML = `
+    <div class="cardInfo">
+        <div class="left">
+        <div class="drinkTitle">
+            <h2>${drink.strDrink}</h2>
+        </div>
+            <div class="image">
+                <img src="${drink.strDrinkThumb}" alt="${drink.strDrink}"></img>
+            </div>
+            </div>
+            <div class="right">
+            <div class="ingredients">
+                <h3>Ingredients</h3>
+                <span>${getIngredient(drink)}</span>
+            </div>
+            <div class="instructions">
+                <h3>Instructions</h3>
+                <p>${drink.strInstructions}</p>
+            </div>
+            </div>
+    </div>
+        
     `
-    document.querySelector('.container').appendChild(section)
+    document.querySelector('.swiper-wrapper').appendChild(div)
 }   
 
 function getIngredient(drink){
@@ -47,10 +69,14 @@ function getIngredient(drink){
                 let ingredientNum = `strIngredient${i}`
                 let ingredient = drink[ingredientNum]
                 if(ingredient === null) break
+                if(ingredient === '') break
                 ingredients.push(ingredient)
              }
               return ingredients.join(', ')
 }
 
-
-
+function reinitSwiper(swiper) {
+    setTimeout(function () {
+     swiper.update();
+    }, 500);
+  }
